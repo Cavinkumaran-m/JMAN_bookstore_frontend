11d58a82512e645be8f76fe56da9c686f22b6733
Cavinkumaran M <cavinkumaran1257@gmail.com>
2024-02-05 22:51:21 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Environmental Setup Done!!!</p><br></br><p>We are ready to go !!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
