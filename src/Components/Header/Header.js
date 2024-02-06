483d7035c74602467c5be30d801396131e71b387
Cavinkumaran M <cavinkumaran1257@gmail.com>
2024-02-06 22:26:40 +0530

    Axios and home page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Header(props) {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <NavLink className="navbar-brand" to="#">
         BookCon
       </NavLink>
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        onClick={handleNavCollapse}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
+      <div
+        className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+        id="navbarNavAltMarkup"
+      >
+        <div className="navbar-nav text-white">
           <NavLink className="nav-item nav-link" to="home">
             Home
           </NavLink>
