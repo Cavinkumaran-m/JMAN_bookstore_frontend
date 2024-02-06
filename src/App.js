bfe6d747f95479358752034573e15b1ad6697c8d
Cavinkumaran M <cavinkumaran1257@gmail.com>
2024-02-06 21:21:42 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/Login_Page";
 import NotFound from "./Pages/NotFound/NotFound";
 import Home from "./Pages/Home/Home";
+import { UserContext } from "./Context/UserContext";
 
 function App() {
+  // Use this context to store data locally
+  const [Store, setStore] = useState({
+    isLoggedIn: false,
+    user_name: null,
+    cart_items: null,
+  });
+
+  // to use context on other components
+  // import { UserContext } from "./Context/UserContext";
+  // import { useContext } from "react";
+  // ...inside component function...
+  // const {Store, setStore} = useContext(UserContext)
+  // console.log(Store.isLoggedIn)
+  // Store.setStore({<new data>})
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <UserContext.Provider value={{ Store, setStore }}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
