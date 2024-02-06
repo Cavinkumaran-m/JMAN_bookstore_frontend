89a41750cf7beff26dac5f0134acf47c8b1dc5c3
Cavinkumaran M <cavinkumaran1257@gmail.com>
2024-02-06 22:55:33 +0530

    final

--- src/Components/BookCard/BoodCardLoader.js
+++ src/Components/BookCard/BoodCardLoader.js
@@ -5,16 +5,23 @@ import { SkeletonTheme } from "react-loading-skeleton";
 
 function BookCardLoader() {
   return (
-    <SkeletonTheme baseColor="#90defd" highlightColor="#00a2e2">
-      <div className="container d-flex bg-white p-4 mt-4 flex-column rounded">
-        <div style={{ height: "500px" }}>
-          <Skeleton width={"100%"} height={"100%"} />
+    <>
+      <div className="container">
+        <div>
+          <Skeleton></Skeleton>
         </div>
-        <br></br>
-        <Skeleton width={"10%"} />
-        <Skeleton count={2} width={"100%"} />
       </div>
-    </SkeletonTheme>
+    </>
+
+    // <SkeletonTheme baseColor="#90defd" highlightColor="#00a2e2">
+    //     <div style={{ height: "100px" }}>
+    //       <Skeleton width={"100%"} height={"100%"} />
+    //     </div>
+    //     <br></br>
+    //     <Skeleton width={"10%"} />
+    //     <Skeleton count={2} width={"100%"} />
+    //   </div>
+    // </SkeletonTheme>
   );
 }
 
