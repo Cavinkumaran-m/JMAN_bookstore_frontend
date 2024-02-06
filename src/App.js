ad584de5001072fef231a35e93f67f30eae4bd68
Cavinkumaran M <cavinkumaran1257@gmail.com>
2024-02-06 21:00:27 +0530

    ck
    naming convention changed
    404 not found page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./Pages/Login Page";
+import LoginPage from "./Pages/Login_Page";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
